0303d6b75091bae9cc97addea19048ba86a50d1c
vivekvarmajgmailcom <vivekvarmaj@gmail.com>
2016-10-14 18:38:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res) {
+    res.send('article one');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app.get('/article-one',function(req,res) {
+    res.send('article 2');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
